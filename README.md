f5958d3a3b0613537fa7eb2febafd38e97058504
Lucas Matheus <lucasmatheus2021@gmail.com>
2020-03-25 23:29:10 -0300

    v0.1
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# to start server at localhost:3000 default and API on localhost:8000
+npm run dev
+
+#to start at localhost:8000
+npm run server
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || 8000;
+
+const { getChampionFreeWeek } = require('./api/champion_v3');
+const { getSummonerInfo, getAllSummonerMatchData } = require('./api/summoner_v4');
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/api/champion/freeweek', getChampionFreeWeek);
+app.get('/api/dashboard/:server/:summonerName', getSummonerInfo);
+app.get('/api/dashboard/match-history', getAllSummonerMatchData);
+
+app.listen(port, () => console.log(`Server running at 'http://localhost:${port}/'`));
